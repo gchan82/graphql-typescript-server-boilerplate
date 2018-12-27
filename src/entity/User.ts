a20672aa76cebd0af15c4d4556f4a722a7f9c5ab
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-12-26 19:07:53 -0800

    eof10.Fix Redis connect error, fix userId!, fix No "user" repo ormconfig

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -3,8 +3,10 @@ import {
     Column,
     BaseEntity,
     PrimaryGeneratedColumn,
+    BeforeInsert,
 }
     from "typeorm";
+import * as uuidv4 from "uuid/v4";
 
 @Entity("users")
 export class User extends BaseEntity {
@@ -12,13 +14,19 @@ export class User extends BaseEntity {
     @PrimaryGeneratedColumn("uuid")
     id: string;
 
-    @Column("varchar", { length: 255, unique: true })
+    @Column("varchar", { length: 255 })
     email: string;
 
     @Column("text")
     password: string;
-
+    
     @Column("boolean", { default: false })
     confirmed: boolean;
+
+    @BeforeInsert()
+    addId() {
+        this.id = uuidv4();
+    }
+
 
 }
--- src/startServer.ts
+++ src/startServer.ts
@@ -2,6 +2,28 @@
 // Part 9 14:32 - last position - line 36
 // Part 9 18:40 - updates to resolvers, graphql-utils.d.ts, startServer.ts
 
+// docker setup:
+// docker redis install: docker pull redis
+// docker run redis-server: docker run --name some-redis -d redis
+// docker redis-cli: docker run -it --link some-redis:redis --rm redis redis-cli -h redis -p 6379
+
+// restart container on next bootup:
+// list container ID: "docker container ls"
+// docker start <container ID>
+// docker stop <container ID>
+// docker rm <container ID>
+// docker redis-cli: docker run -it --link some-redis:redis --rm redis redis-cli -h redis -p 6379
+// try: docker run -p 6379:6379 --name some-redis -d redis
+// ref: https://github.com/andymccurdy/redis-py/issues/852
+// try: docker run -d -p 6379:6379 redis --port 6379 redis-cli -h 127.0.0.1 -p 6379
+
+// ref: https://blogs.msdn.microsoft.com/uk_faculty_connection/2017/02/21/containers-redis-running-redis-on-windows-with-docker/
+
+
+// set bind 0.0.0.0
+// https://stackoverflow.com/questions/41371402/connecting-to-redis-running-in-docker-container-from-host-machine
+
+
 import { importSchema } from 'graphql-import';
 import { GraphQLServer } from 'graphql-yoga';
 import * as path from 'path';
@@ -25,7 +47,6 @@ export const startServer = async () => {
   });
 
   const redis = new Redis();
-
   const server = new GraphQLServer({
     schema: mergeSchemas({ schemas }),
     context: ({ request }) => ({
@@ -37,7 +58,7 @@ export const startServer = async () => {
   server.express.get('/confirm/:id', async (req, res) => {
     const { id } = req.params;
     const userId = await redis.get(id);
-    await User.update({ id: userId }, { confirmed: true });
+    await User.update({ id: userId! }, { confirmed: true });
     res.send('ok');
   });
 
