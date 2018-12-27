74f5da5ffa823a44097399142a0813b45bf42ee7
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-12-27 13:14:43 -0800

    End Part 11.

--- src/utils/createConfirmEmailLink.test.ts
+++ src/utils/createConfirmEmailLink.test.ts
@@ -8,6 +8,7 @@ import { createTypeormConn } from "./createTypeormConn";
 import { User } from '../entity/User';
 
 let userId = '';
+const redis = new Redis();
 
 beforeAll(async () => {
   await createTypeormConn();
@@ -18,15 +19,30 @@ beforeAll(async () => {
   userId = user.id;
 });
 
-test("Make sure createConfirmEmailLink works", async () => {
+describe("test createConfirmEmailLink", () => {
+test("Make sure it confirms user and clears key in Redis", async () => {
+  const redis = new Redis();
   const url = await createConfirmEmailLink(
     process.env.TEST_HOST as string,
     userId,
-    new Redis()
+    redis,
   );
 
   const response = await fetch(url);
   const text = await response.text();
   console.log(text);
+  expect(text).toEqual('ok');
+  const user = await User.findOne({ where: { id: userId } });
+  expect((user as User).confirmed).toBeTruthy();
+  const chunks = url.split('/');
+  const key = chunks[chunks.length - 1];
+  const value = await redis.get(key);
+  expect(value).toBeNull();
+});
 
+test("Sends invalid back if bad id sent", async() => {
+  const response = await fetch(`${process.env.TEST_HOST}/confirm/12083`);
+  const text = await response.text();
+  console.log('invalid');
+})
 });
