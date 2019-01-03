8fcbc720d089ac6f7f5d8c0a5ab26d8116606268
gchan82 <42255277+gchan82@users.noreply.github.com>
2019-01-02 18:04:42 -0800

    End Part 12 - No "User" found.

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,71 +1,20 @@
-// Part 9 10:02 - issues with line 25
-// Part 9 14:32 - last position - line 36
-// Part 9 18:40 - updates to resolvers, graphql-utils.d.ts, startServer.ts
-
-// docker setup:
-// docker redis install: docker pull redis
-// docker run redis-server: docker run --name some-redis -d redis
-// docker redis-cli: docker run -it --link some-redis:redis --rm redis redis-cli -h redis -p 6379
-
-// restart container on next bootup:
-// list container ID: "docker container ls"
-// docker start <container ID>
-// docker stop <container ID>
-// docker rm <container ID>
-// docker redis-cli: docker run -it --link some-redis:redis --rm redis redis-cli -h redis -p 6379
-// try: docker run -p 6379:6379 --name some-redis -d redis
-// ref: https://github.com/andymccurdy/redis-py/issues/852
-// try: docker run -d -p 6379:6379 redis --port 6379 redis-cli -h 127.0.0.1 -p 6379
-
-// ref: https://blogs.msdn.microsoft.com/uk_faculty_connection/2017/02/21/containers-redis-running-redis-on-windows-with-docker/
-
-
-// set bind 0.0.0.0
-// https://stackoverflow.com/questions/41371402/connecting-to-redis-running-in-docker-container-from-host-machine
-
-
-import { importSchema } from 'graphql-import';
 import { GraphQLServer } from 'graphql-yoga';
-import * as path from 'path';
-import * as fs from 'fs';
-import { mergeSchemas, makeExecutableSchema } from 'graphql-tools';
-import { GraphQLSchema } from 'graphql';
-import * as Redis from 'ioredis';
+import {redis} from './redis';
 
 import { createTypeormConn } from './utils/createTypeormConn';
-import { User } from './entity/User';
+import { confirmEmail } from './routes/confirmEmail';
+import { genSchema } from './utils/generateSchema';
 
 export const startServer = async () => {
-  const schemas: GraphQLSchema[] = [];
-  const folders = fs.readdirSync(path.join(__dirname, './modules'));
-  folders.forEach(folder => {
-    const { resolvers } = require(`./modules/${folder}/resolvers`);
-    const typeDefs = importSchema(
-      path.join(__dirname, `./modules/${folder}/schema.graphql`)
-    );
-    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
-  });
-
-  const redis = new Redis();
   const server = new GraphQLServer({
-    schema: mergeSchemas({ schemas }),
+    schema: genSchema(),
     context: ({ request }) => ({
       redis,
       url: request.protocol + '://' + request.get('host')
     })
   });
 
-  server.express.get('/confirm/:id', async (req, res) => {
-    const { id } = req.params;
-    const userId = await redis.get(id);
-    if (userId) {
-      await User.update({ id: userId }, { confirmed: true });
-      await redis.del(id);
-      res.send('ok');
-    } else {
-      res.send('invalid');
-    }
-  });
+  server.express.get('/confirm/:id', confirmEmail);
 
   await createTypeormConn();
   const app = await server.start({
--- src/utils/createConfirmEmailLink.test.ts
+++ src/utils/createConfirmEmailLink.test.ts
@@ -1,7 +1,9 @@
-// part 11 (9:00) - last position. 
+// part 11 (9:00) - fixed.
+// part 12 - 0:00
 
 import * as Redis from 'ioredis';
 import fetch from 'node-fetch';
+import {Connection} from 'typeorm';
 
 import { createConfirmEmailLink } from "./createConfirmEmailLink";
 import { createTypeormConn } from "./createTypeormConn";
@@ -10,8 +12,10 @@ import { User } from '../entity/User';
 let userId = '';
 const redis = new Redis();
 
+let conn: Connection;
+
 beforeAll(async () => {
-  await createTypeormConn();
+  conn = await createTypeormConn();
   const user = await User.create({
     email: "bob5@bob.com",
     password: "awoinaslidtyao"
@@ -19,30 +23,30 @@ beforeAll(async () => {
   userId = user.id;
 });
 
-describe("test createConfirmEmailLink", () => {
-test("Make sure it confirms user and clears key in Redis", async () => {
-  const redis = new Redis();
-  const url = await createConfirmEmailLink(
-    process.env.TEST_HOST as string,
-    userId,
-    redis,
-  );
-
-  const response = await fetch(url);
-  const text = await response.text();
-  console.log(text);
-  expect(text).toEqual('ok');
-  const user = await User.findOne({ where: { id: userId } });
-  expect((user as User).confirmed).toBeTruthy();
-  const chunks = url.split('/');
-  const key = chunks[chunks.length - 1];
-  const value = await redis.get(key);
-  expect(value).toBeNull();
+afterAll(async () => {
+conn.close();
 });
 
-test("Sends invalid back if bad id sent", async() => {
-  const response = await fetch(`${process.env.TEST_HOST}/confirm/12083`);
-  const text = await response.text();
-  console.log('invalid');
-})
-});
+  test("Make sure it confirms user and clears key in Redis", async () => {
+    const redis = new Redis();
+    const url = await createConfirmEmailLink(
+      process.env.TEST_HOST as string,
+      userId,
+      redis,
+    );
+
+    const response = await fetch(url);
+    const text = await response.text();
+    console.log(text);
+    expect(text).toEqual('ok');
+    const user = await User.findOne({ where: { id: userId } });
+    expect((user as User).confirmed).toBeTruthy();
+    const chunks = url.split('/');
+    const key = chunks[chunks.length - 1];
+    const value = await redis.get(key);
+    expect(value).toBeNull();
+  });
+
+  // Documentation:
+  // expect(value).toBeNull()
+  // after confirming user, we should delete key from redis  - redis.del(id)
